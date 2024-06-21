70ca6531931cdc82009846398220e45cd52fd361
devjvla <dev.jvla@gmail.com>
2024-06-21 15:05:29 +0800

    Add documentation; Refactor some repeated code

--- index.js
+++ index.js
@@ -155,11 +155,7 @@ const l_tetris = [
     ],
 ]
 
-const pieces = [
-    s_tetris, t_tetris, z_tetris, o_tetris, j_tetris, i_tetris, l_tetris,
-    l_tetris, s_tetris, t_tetris, z_tetris, o_tetris, j_tetris, i_tetris, 
-    i_tetris, l_tetris, s_tetris, t_tetris, z_tetris, o_tetris, j_tetris,
-];
+const pieces = [s_tetris, t_tetris, z_tetris, o_tetris, j_tetris, i_tetris, l_tetris];
 const piece_values = [1, 2, 3, 4, 5, 6, 7];
 
 let score = 0;
@@ -195,7 +191,7 @@ let board = [
 
 let y_pos              = 0;
 let x_pos              = 4;
-let random_piece_index = Math.floor(Math.random() * pieces.length)
+let random_piece_index = Math.floor(Math.random() * pieces.length);
 let random_piece_pos   = Math.floor(Math.random() * 4);
 let current_piece      = pieces[random_piece_index];
 
@@ -207,17 +203,13 @@ document.addEventListener("DOMContentLoaded", function() {
     // Check if piece is moved left/right/rotated
     document.onkeydown = function(e) {
         if(e.code === "ArrowLeft") {
-            if(x_pos > 0){
-                if(!hasCollision(current_piece[random_piece_pos], y_pos, x_pos - 1)){
-                    x_pos -= 1;
-                }
+            if(x_pos > 0 && !hasCollision(current_piece[random_piece_pos], y_pos, x_pos - 1)){
+                x_pos -= 1;
             }
-        } 
+        }
         else if (e.code == "ArrowRight") {
-            if(x_pos < (10 - current_piece[random_piece_pos][0].length)){
-                if(!hasCollision(current_piece[random_piece_pos], y_pos, x_pos + 1)){
-                    x_pos += 1;
-                }
+            if(x_pos < (10 - current_piece[random_piece_pos][0].length) && !hasCollision(current_piece[random_piece_pos], y_pos, x_pos + 1)){
+                x_pos += 1;
             }
         }
         else if (e.code == "ArrowDown") {
@@ -227,10 +219,8 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         else if (e.code == "KeyZ" || e.code == "KeyX") {
             if(e.code == "KeyZ") {
-                if((random_piece_pos + 1) > 3) {
-                    if(!hasCollision(current_piece[0], y_pos, x_pos)){
-                        random_piece_pos = 0
-                    }
+                if((random_piece_pos + 1) > 3 && !hasCollision(current_piece[0], y_pos, x_pos)) {
+                    random_piece_pos = 0
                 }
                 else {
                     if(!hasCollision(current_piece[random_piece_pos + 1], y_pos, x_pos)){
@@ -238,10 +228,8 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
                 }
             } else {
-                if((random_piece_pos - 1) < 0) {
-                    if(!hasCollision(current_piece[0], y_pos, x_pos)){
-                        random_piece_pos = 3;
-                    }
+                if((random_piece_pos - 1) < 0 && !hasCollision(current_piece[0], y_pos, x_pos)) {
+                    random_piece_pos = 3;
                 }
                 else {
                     if(!hasCollision(current_piece[random_piece_pos - 1], y_pos, x_pos)){
@@ -251,14 +239,10 @@ document.addEventListener("DOMContentLoaded", function() {
             }
 
             // Adjust x_pos of piece if it doesn't fit in the board
-            if(current_piece[random_piece_pos][0].length > (board[y_pos].length - x_pos)){
-                x_pos = board[y_pos].length - current_piece[random_piece_pos][0].length;
-            }
-
+            x_pos = adjustXYPos(current_piece[random_piece_pos], x_pos, y_pos, "x_pos");
+            
             // Adjust y_pos of piece if it doesn't fit in the board
-            if(current_piece[random_piece_pos].length > (board.length - y_pos)){
-                y_pos = board.length - current_piece[random_piece_pos].length;
-            }
+            y_pos = adjustXYPos(current_piece[random_piece_pos], x_pos, y_pos, "y_pos");
         }
         else if (e.code == "Space") {
             let new_y_pos = hardDrop(current_piece[random_piece_pos], y_pos, x_pos);
@@ -297,14 +281,15 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 600);
 });
 
+// This function will render the Tetris game board
 function renderBoard() {
     let boardHTML = "";
 
     for(const [rowCount, row] of board.entries()) {
         boardHTML += `<div id='row_${rowCount}' class='boardRow'>`;
 
-        row.forEach((value, index) => {
-            boardHTML += `<div class='rowBlock' data-status='${value}' style='background-color: ${TETRIS_STYLES[`tetris_${value}`]};'></div>`;
+        row.forEach((value) => {
+            boardHTML += `<div class='rowBlock' style='background-color: ${TETRIS_STYLES[`tetris_${value}`]};'></div>`;
         });
 
         boardHTML += "</div>";
@@ -313,14 +298,17 @@ function renderBoard() {
     return boardHTML;
 }
 
+// This function will update the DOM to display the blocks of a Tetris piece
+// depending on the y_pos and x_pose given. This will also update the DOM to display a highlight
+// identical to the Tetris piece to show where it can be droppped with no collision.
 function drawPiece(piece, y_pos, x_pos, isHighlight = false) {
     for(let rowCount = 0; rowCount < piece.length; rowCount++) {
         for(let blockCount = 0, start_pos = x_pos; blockCount < piece[rowCount].length; blockCount++) {
             let current_block = document.getElementById(`row_${y_pos}`).children[start_pos];
 
+            // Don't update DOM if piece block is 0. 0 means empty space
             if(piece[rowCount][blockCount] != 0){
                 let tetris_style_value = isHighlight ? "highlight" : piece[rowCount][blockCount];
-                current_block.setAttribute("data-status", tetris_style_value);
                 current_block.setAttribute("style", `background-color: ${TETRIS_STYLES[`tetris_${tetris_style_value}`]};`);
             }
 
@@ -337,9 +325,12 @@ function drawPieceHighlight(piece, y_pos, x_pos) {
     drawPiece(piece, new_y_pos, x_pos, true);
 }
 
+// This function will populate the board array with values of piece array
+// according to y_pos and x_pos given
 function plotPiece(piece, y_pos, x_pos) {
     for(let rowCount = 0; rowCount < piece.length; rowCount++) {
         for(let blockCount = 0, start_pos = x_pos; blockCount < piece[rowCount].length; blockCount++) {
+            // Only update board block if it's empty. 0 = empty.
             if(board[y_pos][start_pos] == 0) {
                 board[y_pos][start_pos] = piece[rowCount][blockCount];
             }
@@ -351,19 +342,18 @@ function plotPiece(piece, y_pos, x_pos) {
     }
 }
 
+// This function will attempt to plot piece. If a piece's block
+// is placed on a board[y_pos][x_pos] with non-zero value, it is a collision.
 function hasCollision(piece, y_pos, x_pos) {
     // Adjust x_pos of piece if it doesn't fit in the board
-    if(piece[0].length > (board[y_pos].length - x_pos)){
-        x_pos = board[y_pos].length - piece[0].length;
-    }
+    x_pos = adjustXYPos(piece, x_pos, y_pos, "x_pos");
 
     // Adjust y_pos of piece if it doesn't fit in the board
-    if(piece.length > (board.length - y_pos)){
-        y_pos = board.length - piece.length;
-    }
+    y_pos = adjustXYPos(piece, x_pos, y_pos, "y_pos");
 
     for(let rowCount = 0; rowCount < piece.length; rowCount++) {
         for(let blockCount = 0, start_pos = x_pos; blockCount < piece[rowCount].length; blockCount++) {
+            // If current piece block is not empty and current board block is not empty
             if(piece[rowCount][blockCount] != 0 && board[y_pos][start_pos] != 0){
                 return true;
             }
@@ -377,9 +367,34 @@ function hasCollision(piece, y_pos, x_pos) {
     return false;
 }
 
+// This function will check if tetris piece will fit on the board based on the y_pos and x_pos.
+// Adjust y_pos or x_pos if it doesn't fit.
+function adjustXYPos(piece, x_pos, y_pos, type) {
+    let pos = type == "x_pos" ? x_pos : y_pos;
+
+    const posAdjustments = {
+        "x_pos": () => {
+            if(piece[0].length > (board[y_pos].length - x_pos)){
+                pos = board[y_pos].length - piece[0].length;
+            }
+        },
+        "y_pos": () => {
+            if(piece.length > (board.length - y_pos)){
+                pos = board.length - piece.length;
+            }
+        }
+    }
+
+    posAdjustments[type]();
+
+    return pos;
+}
+
+// This function will return the maximum possible y_pos a piece can be dropped
+// considering that there will be no collisions
 function hardDrop(piece, y_pos, x_pos) {
     for(let boardRow = y_pos; boardRow < board.length; boardRow++) {
-        if(!(piece.length < (board.length - boardRow)) || hasCollision(piece, boardRow + 1, x_pos)) {
+        if(piece.length >= (board.length - boardRow) || hasCollision(piece, boardRow + 1, x_pos)) {
             return boardRow;
         }
     }
@@ -387,6 +402,10 @@ function hardDrop(piece, y_pos, x_pos) {
     return boardRow;
 }
 
+
+// This function will check all rows of the board 2D array.
+// Remove entire row if it doesn't include at least one 0 then unshift an entire row
+// This will also calculate the score depending on the lines cleared
 function clearLines() {
     const score_label = document.getElementById("score");
     let linesCount = 0;
